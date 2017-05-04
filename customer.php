e0745206f997e662da0fdfb497957ce06f6c46ff
Mona subaih <m.subaih@rozn.org>
2017-05-04 12:05:14 +0300

    pass

--- customer.php
+++ customer.php
@@ -25,8 +25,8 @@ $postCustomerDataset ['customer']['verified_email'] = true;
 $postCustomerDataset ['customer']['addresses'] = array();
 $postCustomerDataset ['customer']['addresses'][0]['first_name'] = $customerDataSet['first_name'];
 $postCustomerDataset ['customer']['addresses'][0]['last_name'] = $customerDataSet['last_name'];
-$postCustomerDataset ['customer']['password'] = $customerDataSet['passwordfield'];
-$postCustomerDataset ['customer']['password_confirmation'] = $customerDataSet['passwordfield'];
+$postCustomerDataset ['customer']['password'] = $customerDataSet['password'];
+$postCustomerDataset ['customer']['password_confirmation'] = $customerDataSet['password'];
 $postCustomerDataset ['customer']['send_email_welcome'] = true;
 ////Send to shopify to update Order
 $customerToUpdate = json_encode($postCustomerDataset);

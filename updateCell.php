f93c986bbe7cc68fc95c904f5f29f0021646bd88
Mona subaih <m.subaih@rozn.org>
2017-06-25 05:11:09 +0300

    test

--- updateCell.php
+++ updateCell.php
@@ -48,7 +48,8 @@ if (count($values) == 0) {
 } else {
   foreach ($values as $row) {
       $count = $count - 1;
-      echo $row[3];
+      var_dump($row);
+      break;
     // Print columns A and E, which correspond to indices 0 and 4.
     if($row[3] === $data['email']){
         echo $row[3];

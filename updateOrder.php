01868701f5c02ba53ba2298ec53c9763796c68ed
Hussam El Kurd <h.kurd@rozn.org>
2017-08-09 18:39:33 +0300

    test

--- updateOrder.php
+++ updateOrder.php
@@ -1,12 +1,7 @@
 <?php
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-error_reporting(E_ALL);
-session_start();
 die('hello Heroku');
+
+error_reporting(E_ALL);
 //global parameter
 
 define('SHOPIFY_URL', "");

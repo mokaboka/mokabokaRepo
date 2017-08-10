79541a51d5dc66af79cc2110bf674e9f0e4230fe
Hussam El Kurd <h.kurd@rozn.org>
2017-08-10 08:09:07 +0300

    Update problems

--- updateOrder.php
+++ updateOrder.php
@@ -1,16 +1,18 @@
 <?php
 
-error_reporting(0);
 //global parameter
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 //header('content-type: application/json; charset=utf-8');
 
-
-define('SHOPIFY_URL', "https://c405ef226e3e07c4eb80fcbe1b85712d:61f81d985ec32c6f6c674b7e809c1e19@selfmadeclub.myshopify.com/admin/orders.json");
+define('SHOPIFY_URL', "https://c405ef226e3e07c4eb80fcbe1b85712d:61f81d985ec32c6f6c674b7e809c1e19@selfmadeclub.myshopify.com/admin/orders");
 ////////////////////////////////////////////////////////////////
-
+//$orderDataSet['order_number'] = "#1028";
+//$orderDataSet ['hat_size'] = "XX";
+//$orderDataSet ['shirt_size'] = "sxs";
+//$orderDataSet ['website_address'] = "xss";
 $orderDataSet = json_decode(file_get_contents('php://input'), true);
+
 if(sizeof($orderDataSet) > 0 && $orderDataSet['order_number'] != ''){
   $headers = array(
       'Accept: application/json',
@@ -18,32 +20,38 @@ if(sizeof($orderDataSet) > 0 && $orderDataSet['order_number'] != ''){
   );
 //get The order ID
   $orderNumber = str_replace("#","",$orderDataSet['order_number']);
-  $ch1 = curl_init();
-  $shopifyParamsURL = "?name=".$orderNumber."&status=any";
-  curl_setopt($ch1, CURLOPT_URL, SHOPIFY_URL . $shopifyParamsURL);
-  curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($ch1, CURLOPT_TIMEOUT, 15);
-  curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch1, CURLOPT_SSL_VERIFYHOST, 2);
-  $response = curl_exec($ch1);
-  $responseArr = json_decode($response, true);
 
+  $options = array(
+    CURLOPT_RETURNTRANSFER => true,   // return web page
+    CURLOPT_HEADER         => false,  // don't return headers
+    CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+    CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+    CURLOPT_ENCODING       => "",     // handle compressed
+    CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+    CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+    CURLOPT_TIMEOUT        => 120,    // time-out on response
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_SSL_VERIFYHOST => 2
+);
+$shopifyParamsURL = ".json?name=".$orderNumber."&status=any";
+
+  $ch1 = curl_init(SHOPIFY_URL . $shopifyParamsURL);
+  curl_setopt_array($ch1, $options);
+  $response  = curl_exec($ch1);
+  curl_close($ch1);
 
 
   if($response==false){
-    echo json_encode(array("success" => false, 'response'=> $response)));
+    echo json_encode(array("success" => false, 'response'=> $response));
     exit;
   }
-  else {
-    echo 'hhhhhhhh';
-    exit;
-  }
-  }/*
+
   else{
     //$shopifyParamsURL = $orderDataSet['id'] . ".json";
+    $responseArr = json_decode($response, true);
 
     //array post Parameter
-    $postNoteData ['order']['id'] = $responseArr['order']['id'] ;
+    $postNoteData ['order']['id'] = $responseArr['orders'][0]['id'] ;
     $postNoteData ['order']['note_attributes']['hat_size'] = $orderDataSet['hat_size'];
     $postNoteData ['order']['note_attributes']['shirt_size'] = $orderDataSet['shirt_size'];
     $postNoteData ['order']['note_attributes']['website_address'] = $orderDataSet['website_address'];
@@ -63,20 +71,33 @@ if(sizeof($orderDataSet) > 0 && $orderDataSet['order_number'] != ''){
     fwrite($fp, $orderToUpdate);
     fseek($fp, 0);
 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, SHOPIFY_URL . $shopifyParamsURL);
-    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_INFILE, $fp); // file pointer
-    curl_setopt($ch, CURLOPT_INFILESIZE, strlen($orderToUpdate));
-    curl_setopt($ch, CURLOPT_PUT, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-    $xml_response = curl_exec($ch);
-    if($xml_response!=false){
-        echo(json_encode(array('success' => true, 'response'=> $xml_response)));
+    $options = array(
+      CURLOPT_RETURNTRANSFER => true,   // return web page
+      //CURLOPT_HEADER         => false,  // don't return headers
+      CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+      CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+      CURLOPT_ENCODING       => "",     // handle compressed
+      CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+      CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+      CURLOPT_TIMEOUT        => 120,    // time-out on response
+      CURLOPT_SSL_VERIFYPEER => false,
+      CURLOPT_SSL_VERIFYHOST => 2,
+      CURLOPT_BINARYTRANSFER => true,
+      CURLOPT_HTTPHEADER => $headers,
+      CURLOPT_INFILE => $fp,
+      CURLOPT_PUT => 1,
+
+  );
+    $shopifyParamsURL = "/".$responseArr['orders'][0]['id'].".json";
+    $ch = curl_init(SHOPIFY_URL.$shopifyParamsURL);
+    curl_setopt_array($ch, $options);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    if($response!=false){
+        echo json_encode(array('success' => true, 'response'=> $response));
         exit;
     }
+
   }
 }

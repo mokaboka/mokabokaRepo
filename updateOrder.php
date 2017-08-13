8fa89eb4652ce07f978dc55ef50248ce34e5e41c
Hussam El Kurd <h.kurd@rozn.org>
2017-08-13 08:54:28 +0300

    Update the order

--- updateOrder.php
+++ updateOrder.php
@@ -13,7 +13,7 @@ define('SHOPIFY_URL', "https://c405ef226e3e07c4eb80fcbe1b85712d:61f81d985ec32c6f
 //$orderDataSet ['shirt_size'] = "sxs";
 //$orderDataSet ['website_address'] = "xss";
 $orderDataSet = json_decode(file_get_contents('php://input'), true);
-
+/*
 if(sizeof($orderDataSet) > 0 && $orderDataSet['order_number'] != ''){
   $headers = array(
       'Accept: application/json',
@@ -101,4 +101,60 @@ $shopifyParamsURL = ".json?name=".$orderNumber."&status=any";
     }
 
   }
+}
+*/
+if(sizeof($orderDataSet) > 0 && $orderDataSet['order_number'] != ''){
+  $headers = array(
+      'Accept: application/json',
+      'Content-Type: application/json',
+  );
+
+//array post Parameter
+$postNoteData ['order']['id'] = $responseArr['orders'][0]['id'] ;
+$postNoteData ['order']['note_attributes']['hat_size'] = $orderDataSet['hat_size'];
+$postNoteData ['order']['note_attributes']['shirt_size'] = $orderDataSet['shirt_size'];
+$postNoteData ['order']['note_attributes']['website_address'] = $orderDataSet['website_address'];
+
+
+////Send to shopify to update Order
+
+$orderToUpdate = json_encode($postNoteData);
+
+//use a max of 256KB of RAM before going to disk
+
+
+$fp = fopen('php://temp/maxmemory:256000', 'w');
+if (!$fp) {
+    die('could not open temp memory data');
+}
+fwrite($fp, $orderToUpdate);
+fseek($fp, 0);
+
+$options = array(
+  CURLOPT_RETURNTRANSFER => true,   // return web page
+  //CURLOPT_HEADER         => false,  // don't return headers
+  CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+  CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+  CURLOPT_ENCODING       => "",     // handle compressed
+  CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+  CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+  CURLOPT_TIMEOUT        => 120,    // time-out on response
+  CURLOPT_SSL_VERIFYPEER => false,
+  CURLOPT_SSL_VERIFYHOST => 2,
+  CURLOPT_BINARYTRANSFER => true,
+  CURLOPT_HTTPHEADER => $headers,
+  CURLOPT_INFILE => $fp,
+  CURLOPT_PUT => 1,
+
+);
+$shopifyParamsURL = "/".$responseArr['orders'][0]['id'].".json";
+$ch = curl_init(SHOPIFY_URL.$shopifyParamsURL);
+curl_setopt_array($ch, $options);
+
+$response = curl_exec($ch);
+curl_close($ch);
+if($response!=false){
+    echo json_encode(array('success' => true, 'response'=> $response));
+    exit;
+}
 }
